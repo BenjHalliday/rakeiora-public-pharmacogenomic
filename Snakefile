f05ac7ae57b5fe62b9fcaffae9fe958645be829e
BenjHalliday <benjamin.halliday@otago.ac.nz>
2023-03-13 14:07:08 +1300

    modifications for handling ALEA data

--- Snakefile
+++ Snakefile
@@ -16,18 +16,20 @@ BCFSingularity   = config["BCFSingularity"]
 pharmgkb         = config["pharmgkb"]
 pharmvar         = config["pharmvar"]
 inputvcffile     = config["inputvcffile"]
-druglistfile     = config["druglistfile"]
-idlistfile       = config["idlistfile"]
+#druglistfile     = config["druglistfile"]
+#idlistfile       = config["idlistfile"]
+druglistpres     = config["druglistpres"]
+druglistdisp     = config["druglistdisp"]
 
 ####################     Initialise       ###########################
 
 rule all:
     input:
-        expand(outdir + '{allele}_{drug}_Report.html', allele= ALLELES, drug= DRUGS)
-        ,expand(outdir + '{allele}.done', allele= ALLELES)
+        expand(outdir + '{allele}_{drug}_Report.html', allele= ALLELES, drug= DRUGS),
+        expand(outdir + '{allele}.done', allele= ALLELES)
 
 
-#####################     Module 2 - Genotype Manipulation       ###########################
+#####################     Module 1 - Genotype Manipulation       ###########################
 
 rule makeAlleleFiles:
     input:
@@ -39,9 +41,6 @@ rule makeAlleleFiles:
     shell:
         "cp {input.vcf} {output.o1} &> {log}"
 
-# Split out haplotype variants from raw VCF based on pharmvar VCF,
-# need to split a gene specific VCF out first -
-# need to figure out how to manipulate ALLELES array into GENE array
 rule GeneVCFtoGenotype:
     input:
         outdir + "{allele}.vcf.gz"
@@ -76,35 +75,35 @@ rule HaplotypetoGraph:
     shell:
         "Rscript bin/HaplotypetoGraph.R $PWD {outdir} {wildcards.allele} &> {log}"
 
-#####################     Module 3 - Haplotype by Drug Annotations     ###########################
+#####################     Module 2 - Haplotype by Drug Annotations     ###########################
 
-rule makeIDListFile:
-    input:
-        vcf = resourcedir + inputvcffile
-    output:
-        o1 = outdir + idlistfile
-    container:
-        singdir + BCFSingularity
-    shell:
-        "bcftools query -l {input.vcf} > {output.o1}"
-
-rule GetDrugList:
-    input:
-        i1 = pharmgkb
-        ,i2 = outdir + idlistfile
-    output:
-        o1 = outdir + druglistfile
-    log: 
-        logdir + "GetDrugList.log"
-    container:
-        singdir + RSingularity
-    shell:
-        "Rscript bin/MockDrugData.R $PWD {input.i1} {input.i2} {output.o1} &> {log}"
+# rule makeIDListFile:
+#     input:
+#         vcf = resourcedir + inputvcffile
+#     output:
+#         o1 = outdir + idlistfile
+#     container:
+#         singdir + BCFSingularity
+#     shell:
+#         "bcftools query -l {input.vcf} > {output.o1}"
+# 
+# rule GetDrugList:
+#     input:
+#         i1 = pharmgkb,
+#         i2 = outdir + idlistfile
+#     output:
+#         o1 = outdir + druglistfile
+#     log: 
+#         logdir + "GetDrugList.log"
+#     container:
+#         singdir + RSingularity
+#     shell:
+#         "Rscript bin/MockDrugData.R $PWD {input.i1} {input.i2} {output.o1} &> {log}"
 
 rule HaplotypeDrugAnnotation:
     input:
-        outdir + "{allele}_Haplotype.tsv"
-        ,pharmgkb
+        outdir + "{allele}_Haplotype.tsv",
+        pharmgkb
     output:
         outdir + "{allele}_Haplotype_PharmGKBAnnotation.tsv"
     log: 
@@ -112,39 +111,43 @@ rule HaplotypeDrugAnnotation:
     shell:
         "bin/HaplotypeDrugAnnotation.sh $PWD {outdir} {pharmgkb} {wildcards.allele} &> {log}"
 
-rule HaplotypeDrugAnnotationtoSampleDrugAnnotation:
-    input:
-        outdir + "{allele}_Haplotype_PharmGKBAnnotation.tsv"
-        ,i2 = outdir + druglistfile
-    output:
-        outdir + "{allele}_SampleHaplotype_Annotation_Summary.tsv"
-    log: 
-        logdir + "HaplotypeDrugAnnotationtoSampleDrugAnnotation.{allele}.log"
-    container:
-        singdir + RSingularity
-    shell:
-        "Rscript bin/HaplotypeDrugAnnotationtoSampleDrugAnnotation.R $PWD {outdir} {wildcards.allele} {input.i2} &> {log}"
+#rule HaplotypeDrugAnnotationtoSampleDrugAnnotation:
+#    input:
+#        outdir + "{allele}_Haplotype_PharmGKBAnnotation.tsv",
+#        i2 = outdir + druglistpres,
+#        i3 = outdir + druglistdisp
+#    output:
+#        outdir + "{allele}_SampleHaplotype_Annotation_Summary.tsv"
+#    log: 
+#        logdir + "HaplotypeDrugAnnotationtoSampleDrugAnnotation.{allele}.log"
+#    container:
+#        singdir + RSingularity
+#    shell:
+#        "Rscript bin/HaplotypeDrugAnnotationtoSampleDrugAnnotation.R $PWD {outdir} {wildcards.allele} {input.i2} {input.i3} &> {log}"
 
 rule HaplotypeDrugAnnotationtoSpecificDrugAnnotation:
     input:
-        i1 = outdir + druglistfile
-        ,i2 = outdir + "{allele}_Haplotype_PharmGKBAnnotation.tsv"
-    output:
+        i1 = outdir + "{allele}_Haplotype_PharmGKBAnnotation.tsv",
+        i2 = outdir + druglistpres,
+        i3 = outdir + druglistdisp
+   output:
         outdir + "{allele}_Haplotype_Distribution_{drug}.pdf",
-        outdir + "{allele}_Haplotype_Annotation_Summary_Prescribed_{drug}.tsv"
+        outdir + "{allele}_Haplotype_Annotation_Summary_Prescribed_{drug}.tsv",
+        outdir + "{allele}_Haplotype_Annotation_Summary_Dispensed_{drug}.tsv"
     log: 
         logdir + "HaplotypeDrugAnnotationtoSpecificDrugAnnotation.{allele}.{drug}.log"
     container:
         singdir + RSingularity
     shell:
-        "Rscript bin/HaplotypeDrugAnnotationtoSpecificSampleDrugAnnotation.R $PWD {outdir} {wildcards.allele} {wildcards.drug} {input.i1} &> {log}"
+        "Rscript bin/HaplotypeDrugAnnotationSampleDrugAnnotation.R $PWD {outdir} {wildcards.allele} {wildcards.drug} {input.i2} {input.i3} &> {log}"
 
-#####################     Report and Cleanup      ###########################
+#####################     Module 3 - Report and Cleanup      ###########################
 
 rule report:
     input:
         outdir + "{allele}_Haplotype_Distribution_{drug}.pdf",
         outdir + "{allele}_Haplotype_Annotation_Summary_Prescribed_{drug}.tsv",
+        outdir + "{allele}_Haplotype_Annotation_Summary_Dispensed_{drug}.tsv",
         outdir + "{allele}_Haplotype_Distribution.pdf"
     output:
         outdir + "{allele}_{drug}_Report.html"
--- bin/HaplotypeDrugAnnotationtoSampleDrugAnnotation.R
+++ bin/HaplotypeDrugAnnotationtoSampleDrugAnnotation.R
@@ -11,7 +11,9 @@ print(args)
 dir <- args[1]
 outdir <- args[2]
 allele <- args[3]
-filenm <- args[4]
+drug <- args[4]
+presfile <- args[5]
+dispfile <- args[6]
 
 ###   Parameters  ### 
 
@@ -25,11 +27,10 @@ print(paste0("Directory Set to ", dir))
 
 ###      Packages         ### 
 
-library(tidyverse,quietly=TRUE,warn.conflicts=FALSE)
-#if(!require(tidyverse,quietly=TRUE,warn.conflicts=FALSE)){
-#    install.packages("tidyverse",quiet=TRUE)
-#    library(tidyverse,quietly=TRUE,warn.conflicts=FALSE)
-#}
+if(!require(tidyverse,quietly=TRUE,warn.conflicts=FALSE)){
+    install.packages("tidyverse",quiet=TRUE)
+    library(tidyverse,quietly=TRUE,warn.conflicts=FALSE)
+}
 
 print("Packages Loaded")
 
@@ -37,7 +38,6 @@ print("Packages Loaded")
 ###                                               Load in Data                                                  ####
 ####################################################################################################################
 
-read.table(filenm, header = T,sep="\t")  -> Drugs
 read.table(paste0(outdir,allele,"_Haplotype.tsv"), header = T,sep="\t")  -> Haplotypes
 read.delim(paste0(outdir,allele,"_Haplotype_PharmGKBAnnotation.tsv"), header = T,sep="\t",quote="")  -> HaplotypeAnnotations
 colnames(HaplotypeAnnotations) <- c("ID","StudyHaplotype","Gene","Drug","PMID","Phenotype","Significance","Notes","Sentence","Alleles","Population")
@@ -46,14 +46,74 @@ colnames(HaplotypeAnnotations) <- c("ID","StudyHaplotype","Gene","Drug","PMID","
 ###                                                Data Manipulation                                            ####
 ####################################################################################################################
 
-left_join(Haplotypes,Drugs) -> Sample_Index
-as.character(HaplotypeAnnotations$Drug) %>% gsub("\\\"","",. ) -> HaplotypeAnnotations$Drug
-HaplotypeAnnotations %>% separate_rows(., Drug, convert = TRUE) %>% unique() -> HaplotypeAnnotations_DrugSplit
-Sample_Index %>% filter(Drug %in% HaplotypeAnnotations_DrugSplit$Drug) -> Sample_Index_HapDrug
-Sample_Index_HapDrug %>% filter(Genotype != 0) -> Sample_Index_HapDrug_NonRef
-left_join(Sample_Index_HapDrug_NonRef,HaplotypeAnnotations_DrugSplit,by="Drug") -> Sample_Index_HapDrug_NonRef_Annotations
+groups <- c("Prescribed","Dispensed")
+prefixes <- c("pres","disp")
 
-write.table(Sample_Index_HapDrug_NonRef_Annotations, file = paste0(outdir,allele,"_SampleHaplotype_Annotation_Summary.tsv"),sep="\t",row.names=F)
+for(group in groups) { 
+	prefix <- prefixes[which(groups==group)]
+
+	read.table(get(paste0(prefix,"file")), header = FALSE, sep=",") -> drugs
+	colnames(drugs) <- c("ID","ALEA_ID","Drug")
+
+	as.character(HaplotypeAnnotations$Drug) %>% gsub("\\\"","",. ) -> HaplotypeAnnotations$Drug
+	HaplotypeAnnotations %>% separate_rows(., Drug, convert = TRUE) %>% unique() -> HaplotypeAnnotations_DrugSplit
+
+	left_join(Haplotypes,drugs) -> Sample_Index
+	write.table(Sample_Index, file = paste0(outdir,allele,"_Haplotype_Annotation_Summary_",group,".tsv"),sep="\t",row.names=F)
+
+	Sample_Index %>% filter(Drug %in% HaplotypeAnnotations_DrugSplit$Drug) -> Sample_Index_HapDrug
+	
+	### Filtered Annotations
+	HaplotypeAnnotations_DrugSplit %>% filter(Drug == drug) -> HaplotypeAnnotations_DrugSplit_Filter
+	Sample_Index_HapDrug %>% filter(Drug == drug) -> Sample_Index_HapDrug_Filter
+	
+	left_join(Sample_Index_HapDrug_Filter,HaplotypeAnnotations_DrugSplit_Filter,by="Drug") -> Sample_Index_HapDrug_Annotations
+	write.table(Sample_Index_HapDrug_Annotations, file = paste0(outdir,allele,"_Haplotype_Annotation_Summary_",group,"_",drug,".tsv"),sep="\t",row.names=F)
+}
+
+####################################################################################################################
+###                                                    Plot                                                     ####
+####################################################################################################################
+
+for(group in groups) { 
+	prefix <- prefixes[which(groups==group)]
+
+	Sample_Index %>% select(Sample) %>% pull() %>% unique() -> All_ID
+	Sample_Index_HapDrug %>% filter(Drug == drug) %>% select(Sample) %>% pull() -> Hit_ID
+	setdiff(All_ID,Hit_ID) -> Miss_ID
+	
+	Haplotypes$DrugStatus[Haplotypes$Sample %in% Hit_ID] <- paste0(group,"\n",str_to_title(drug))
+	Haplotypes$DrugStatus[Haplotypes$Sample %in% Miss_ID] <- paste0("Not ",group,"\n",str_to_title(drug))
+	
+	factor(Haplotypes$DrugStatus,levels=c(paste0(group,"\n",str_to_title(drug)),paste0("Not ",group,"\n",str_to_title(drug)))) -> Haplotypes$DrugStatus
+	
+	Haplotypes$Genotype %>% gsub(0,"Homozygous-Reference",.) %>% gsub(1,"Heterozygous",.) %>% gsub(2,"Homozygous-Alternative",.) %>% factor(.,levels=c("Homozygous-Reference","Heterozygous","Homozygous-Alternative")) -> Haplotypes$Genotype_Written
+	
+	plot <- ggplot(data=Haplotypes,aes(x=Genotype_Written)) +
+	    facet_wrap(~ DrugStatus,drop = FALSE) +
+	    scale_y_continuous(expand = c(0,0),limits=c(0,nrow(Haplotypes))) +
+	    geom_bar(stat="count") +
+	    ggtitle(paste0(allele," Haplotype Distribution by ", str_to_title(drug)," Status")) +
+	    ylab("Genotype Count") + 
+	    xlab(paste0(allele," Genotype")) +
+	    scale_x_discrete(drop=FALSE) +
+	    theme(
+	        panel.grid = element_blank(),
+	        plot.title = element_text(size=50, face="bold", hjust = 0.5, margin = margin(t = 40, r = 0, b = 50, l = 0)),
+	        axis.text.y = element_text(angle = 0,size=34, margin = margin(t = 0, r = 10, b = 0, l = 0)),
+	        axis.text.x = element_text(angle = 0,size=22,vjust = 0.5, margin = margin(t = 10, r = 0, b = 0, l = 0)),
+	        axis.title.y = element_text(size=38, margin = margin(t = 0, r = 30, b = 0, l = 20)),
+	        axis.title.x = element_text(size=38, margin = margin(t = 30, r = 0, b = 20, l = 0)),
+	        axis.ticks=element_blank(),
+	        plot.background = element_rect(fill = 'white', colour = 'white'),
+	        strip.text.x = element_text(size=38, margin = margin(t = 20, r = 0, b = 20, l = 0)),
+	        panel.border = element_blank(),
+	        panel.background = element_blank(),
+	        strip.background = element_rect(fill="white",colour="white",linetype="solid"),
+	        axis.line = element_line(colour = "black")
+	    )
+	ggsave(plot=plot,filename=paste0(outdir,allele,"_Haplotype_Distribution_",group,"_",drug,".pdf"),device="pdf", width = 60, height = 40, units = "cm",dpi = 640)
+}
 
 ####################################################################################################################
 ###                                                      Fin                                                    ####
